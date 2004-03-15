1be429c1f0b908ff6eea2c13a5e364862f8e0be1
jaromil <jaromil@129b00e9-8bf7-0310-bee9-bd51b64996e4>
2004-03-15 14:37:17 +0000

    some more fixes and cleanups done last friday, now docking works correctly. left todo: basic config file support and cd eject
    git-svn-id: svn://dyne.org/dynebolic@52 129b00e9-8bf7-0310-bee9-bd51b64996e4

--- dyneI/startup/libdyne.sh
+++ dyneI/startup/libdyne.sh
@@ -59,109 +59,28 @@ loadmod() {
     if [ ! -z "`echo $KMOD_OUT | grep -E $1`" ]; then
 	# skip modules included in /etc/modules.deny
 	act "skipping kernel module $1 (match in KMOD_OUT)"
-    elif [ -z "`grep interactive /proc/cmdline`" ]; then 
-	# go straight, no interaction on module loading
-	echo -n " .  loading kernel module $1 ... " | tee -a $LOG
-	modprobe $1 1>>$LOG 2>>$LOG
-	if [ $? = 0 ]; then
-	    echo "OK" | tee -a $LOG
-	else
-	    echo
-	    error "ERROR loading $1 kernel module"
-	fi
-    else
-	# ask before loading each module
-	echo -n "[?] load kernel module $1 " | tee -a $LOG
+	return
+    fi
+    # in interactive mode we ask 
+    if [ "`grep -i interactive /proc/cmdline`" ]; then 
+ 	echo -n "[?] do you want to load kernel module $1 " | tee -a $LOG
 	getkey 10
-	if [ $? = 1 ]; then echo " ... SKIPPED" | tee -a $LOG
-#	elif [ $? = 2 ]; then echo " ... SKIPPED (timeout)" | tee -a $LOG
-	else # YES, if y or any other key but 'n' is typed
-	    modprobe $1 1>>$LOG 2>>$LOG
-	    if [ $? = 0 ]; then
-		echo " ... OK" | tee -a $LOG
-	    else
-		echo
-		error "ERROR loading $1 kernel module"
-	    fi
+	if [ $? = 1 ]; then
+	    echo " ... SKIPPED" | tee -a $LOG
+	    return
+	else
+	    echo " ... LOADED" | tee -a $LOG
 	fi
+    fi 
+    # finally we do it
+    modprobe $1 1>>$LOG 2>>$LOG
+    if [ $? = 0 ]; then
+        act "loaded kernel module $1"
+    else
+        # no error output to console
+	# "i panni sporchi si lavano in famiglia"
+        echo "[!] ERROR loading kernel module $1" >> $LOG
     fi
 }
 
-
-
-
-dyne_add_volume() {
-  # $1 = media type (hdisk|floppy|usbkey|cd)
-  # $2 = mount point
-  VOLNUM=`expr $VOLNUM + 1`
-  notice "adding $VOLNUM $1 volume on $2"
-  WMCFG="/var/run/WMState"
-  if [ -e /var/run/WMNum ]; then
-      WMNUM="`cat /var/run/WMNum`"
-  else WMNUM=1; fi
-
-  case "$1" in
-      "hdisk")
-	  echo "," >> $WMCFG;
-	  echo "{" >> $WMCFG;
-	  echo "Name = \"$HDEV.HardDisk\";" >>$WMCFG;
-	  echo "Lock = yes;" >>$WMCFG;
-	  echo "Autolaunch = no;" >>$WMCFG;
-	  echo "Command = \"xwc /vol/${2}\";" >>$WMCFG;
-	  WMNUM="`expr $WMNUM + 1`"
-	  echo "Position = \"0,${WMNUM}\";" >>$WMCFG;
-	  echo "Forced = no;" >>$WMCFG;
-	  echo "BuggyApplication = no;" >>$WMCFG;
-	  echo "}" >>$WMCFG;
-	  ;;
-      "floppy")
-	  echo "," >>$WMCFG;
-	  echo "{" >> $WMCFG;
-	  echo "Name = \"${2}.FloppyDisk\";" >>$WMCFG
-	  echo "Lock = yes;" >>$WMCFG
-	  echo "Autolaunch = no;" >>$WMCFG
-	  echo "Command = \"xwc /rem/${2}\";" >>$WMCFG
-	  WMNUM="`expr $WMNUM + 1`"
-	  echo "Position = \"0,${WMNUM}\";" >>$WMCFG;
-	  echo "Forced = no;" >>$WMCFG;
-	  echo "BuggyApplication = no;" >>$WMCFG;
-	  echo "}" >>$WMCFG;
-	  ;;
-      "usb")
-	  echo "," >>$WMCFG;
-	  echo "{" >> $WMCFG;
-	  echo "Name = \"${2}.UsbStorage\";" >>$WMCFG
-	  echo "Lock = yes;" >>$WMCFG
-	  echo "Autolaunch = no;" >>$WMCFG
-	  echo "Command = \"xwc /rem/${2}\";" >>$WMCFG
-	  WMNUM="`expr $WMNUM + 1`"
-	  echo "Position = \"0,${WMNUM}\";" >>$WMCFG;
-	  echo "Forced = no;" >>$WMCFG;
-	  echo "BuggyApplication = no;" >>$WMCFG;
-	  echo "}" >>$WMCFG;
-	  ;;
-      "cd")
-	  echo "," >>$WMCFG;
-	  echo "{" >> $WMCFG;
-	  echo "Name = \"${2}.CdRom\";" >>$WMCFG
-	  echo "Lock = yes;" >>$WMCFG
-	  echo "Autolaunch = no;" >>$WMCFG
-	  echo "Command = \"xwc /rem/${2}\";" >>$WMCFG
-	  WMNUM="`expr $WMNUM + 1`"
-	  echo "Position = \"0,${WMNUM}\";" >>$WMCFG;
-	  echo "Forced = no;" >>$WMCFG;
-	  echo "BuggyApplication = no;" >>$WMCFG;
-	  echo "}" >>$WMCFG;
-	  ;;
-
-      *)
-	  error "invalid call to dyne_gen_wmaker_dock() in libdyne.sh"
-	  return 0
-	  ;;
-  esac
-
-  if [ -e /var/run/WMNum ]; then rm -f /var/run/WMNum; fi
-  echo "$WMNUM" > /var/run/WMNum
-
-}
 
