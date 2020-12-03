ff3dfa89c064329b3aac3eeb2240e0bad1e6bc6a
Jaromil <jaromil@dyne.org>
2020-12-03 19:57:02 +0100

    small reorg and some scripts

---
+++ dyneIV/SDK/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# made for use on Devuan Beowulf
+#
+# reqs: zsh debootstrap
+
+if ! [ -r live-sdk ]; then
+	git clone --recursive https://git.devuan.org/devuan-sdk/live-sdk
+fi
+cd live-sdk
+sed -i 's/firmware-linux//' config
+git apply ../live-sdk.patch
+mkdir blends/dynebolic
+cp ../dynebolic-desktop.blend blends/dynebolic/desktop.blend
+cp ../desktop-config ../dynebolic/dyneIV/config blends/dynebolic/
+zsh -f -c 'source sdk && load devuan dynebolic && \
+	release=beowulf && \
+    version=3.0.0 && \
+	bootstrap_complete_base'
+cd -
+
