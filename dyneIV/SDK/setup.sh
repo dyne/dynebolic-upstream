7afd295130da6dab5e4a8be2c381a01228d9914f
Jaromil <jaromil@dyne.org>
2020-12-04 12:02:14 +0100

    SDK small fixes and docs

--- dyneIV/SDK/setup.sh
+++ dyneIV/SDK/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-
+source utils.sh
 cpio=live-sdk/tmp/bootstrap-devuan-amd64-stage4.cpio.gz
 [[ -r $cpio ]] || {
 	print "error: cpio not found $cpio"
@@ -9,12 +9,15 @@ cpio=live-sdk/tmp/bootstrap-devuan-amd64-stage4.cpio.gz
 # decompress cpio in mnt
 mkdir -p mnt
 [[ -r mnt/etc/inittab ]] || {
+	notice "Setting up the stage4 please wait..."
 	pushd mnt
-	zcat $cpio | sudo cpio -id
+	zcat ../$cpio | sudo cpio -id
 	mkdir -p {boot,dev,proc,sys}
 	popd
 }
 
 # setup repos
+./mount.sh
 ./exec.sh linux-libre.chsh
+./umount.sh
 
