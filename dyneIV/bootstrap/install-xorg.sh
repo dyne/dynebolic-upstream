3e853211668035e5e6e20b5956a527faed361bb6
Jaromil <jaromil@dyne.org>
2023-12-04 21:17:22 +0000

    mate packages suggested by antofox

--- dyneIV/bootstrap/install-xorg.sh
+++ dyneIV/bootstrap/install-xorg.sh
@@ -5,4 +5,5 @@ xserver-xorg-core xserver-xorg xinit xterm xserver-xorg-video-dummy \
 xserver-xephyr x11-utils \
 openbox python3-xdg \
 mesa-utils desktop-base devuan-baseconf xdm devuan-xdm-config-override \
-deepsea-icon-theme mate-desktop-environment
+deepsea-icon-theme mate-desktop-environment mate-desktop-environment-extras \
+engrampa atril brisk-mate-applet
