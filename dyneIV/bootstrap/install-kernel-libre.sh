90a069e35a818ad388f614dfb6fe68c8d932ea99
Jaromil <jaromil@dyne.org>
2023-12-05 21:06:34 +0000

    include linux-libre repo and keyring

--- dyneIV/bootstrap/install-kernel-libre.sh
+++ dyneIV/bootstrap/install-kernel-libre.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 FREESH=freesh-archive-keyring_1.1_all.deb
-
-curl https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/f/freesh-archive-keyring/${FREESH} --output ${FREESH}
-dpkg -i /${FREESH}
-rm -f ROOT/${FREESH}
+dpkg -i /usr/src/${FREESH}
+rm -f /usr/src/${FREESH}
 apt-get update -q -y
 apt-get install -q -y linux-libre-6.1*
