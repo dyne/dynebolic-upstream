799ff9e02872cb72ebe4a7e6a2092b57dea37cea
Jaromil <jaromil@dyne.org>
2023-12-05 17:50:22 +0000

    small fixes to the apt-get update

--- dyneIV/bootstrap/install-kernel-libre.sh
+++ dyneIV/bootstrap/install-kernel-libre.sh
@@ -5,5 +5,5 @@ FREESH=freesh-archive-keyring_1.1_all.deb
 curl https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/f/freesh-archive-keyring/${FREESH} --output ${FREESH}
 dpkg -i /${FREESH}
 rm -f ROOT/${FREESH}
-apt-get update
-apt-get -q -y install linux-libre-6.1*
+apt-get update -q -y
+apt-get install -q -y linux-libre-6.1*
