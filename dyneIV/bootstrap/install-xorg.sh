0a25f3b5f590acd9ef60f113bd0145e2f7b82d08
Jaromil <jaromil@dyne.org>
2023-12-06 18:02:28 +0000

    small xorg improvements

--- dyneIV/bootstrap/install-xorg.sh
+++ dyneIV/bootstrap/install-xorg.sh
@@ -6,18 +6,16 @@ xserver-xephyr x11-utils \
 openbox python3-xdg \
 mesa-utils desktop-base devuan-baseconf xdm devuan-xdm-config-override \
 deepsea-icon-theme mate-desktop-environment mate-desktop-environment-extras \
-engrampa atril mate-applet-brisk-menu
+engrampa atril mate-applet-brisk-menu pluma eom mozo
 
 # fallback wm is openbox
 mkdir -p /etc/X11
 echo "exec openbox-session" > /etc/X11/xinitrc
 # xdm logs into mate
 cat <<EOF > /etc/X11/xsessionrc
+pipewire &
+pipewire-pulse &
 wireplumber &
 
-pipewire &
-
-pipewire-pulse &
-
-mate-session
+dbus-launch --exit-with-session mate-session
 EOF
