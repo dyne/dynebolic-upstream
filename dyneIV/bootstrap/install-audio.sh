e9fea2e283a8e0ca454adebf6b1d63cd712aeba0
Jaromil <jaromil@dyne.org>
2023-12-06 18:01:42 +0000

    notes on audio

--- dyneIV/bootstrap/install-audio.sh
+++ dyneIV/bootstrap/install-audio.sh
@@ -3,3 +3,6 @@
 DEBIAN_FRONTEND=noninteractive apt-get -q -y install \
 pipewire-audio pavucontrol pamix \
 audacity hydrogen zynaddsubfx
+
+# https://wiki.gentoo.org/wiki/PipeWire
+# https://gitweb.gentoo.org/repo/gentoo.git/tree/media-video/pipewire/files/gentoo-pipewire-launcher.in-r3
