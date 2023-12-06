26cf8b300f9923c666d476e9f0d51ae4b5a46985
Jaromil <jaromil@dyne.org>
2023-12-06 18:01:57 +0000

    live-boot conf

--- dyneIV/bootstrap/install-live-boot.sh
+++ dyneIV/bootstrap/install-live-boot.sh
@@ -2,4 +2,23 @@
 
 apt-get -q -y install mount live-boot zstd direnv \
 		openrc sysvinit-core psmisc e2fsprogs orphan-sysvinit-scripts
+
+cat <<EOF > /etc/live/boot.conf
+MINIMAL=false
+PERSISTENCE_FSCK=false
+DISABLE_NTFS=true
+DISABLE_FUSE=true
+DISABLE_DM_VERITY=true
+EOF
+
+echo "sd_mod" > /etc/initramfs-tools/modules
+
+# Use Quad9 as default DNS
+echo "nameserver 9.9.9.9" > /etc/resolv.conf
+
+cat <<EOF > /etc/hosts
+127.0.0.1       localhost
+127.0.1.1       dynebolic
+127.0.0.1       ip6-localhost ip6-loopback
+EOF
 update-initramfs -k all -c
