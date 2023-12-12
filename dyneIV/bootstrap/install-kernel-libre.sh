a9f069c9bc7a7237c0d08ef1d2cec67f2d6b20f8
Jaromil <jaromil@dyne.org>
2023-12-12 14:41:11 +0000

    refactoring of build scripts to use modular makefile setup
    GNU make is a powerful DSL, we abuse it a bit in a setup with
    different directories broken down in 3 phases and producing 3 stages
    plus a final ISO file:
    1. bootstrap -> stage1 and stage2
    2. packages -> stage3
    2. iso -> iso file
    
    all results are created in the main dyneIV root and the rootfs is also
    found there and named ROOT, possible to chroot in it and Xephyr

--- dyneIV/bootstrap/install-kernel-libre.sh
+++ dyneIV/bootstrap/install-kernel-libre.sh
@@ -3,5 +3,5 @@
 FREESH=freesh-archive-keyring_1.1_all.deb
 dpkg -i /usr/src/${FREESH}
 rm -f /usr/src/${FREESH}
-apt-get update -q -y
-apt-get install -q -y linux-libre-6.1*
+DEBIAN_FRONTEND=noninteractive apt-get update -q -y
+DEBIAN_FRONTEND=noninteractive apt-get install -q -y linux-libre-6.1*
