c943c1fa555cb36cb99ad3241e801471dfff668e
Jaromil <jaromil@dyne.org>
2023-12-22 21:33:35 +0000

    more friendly makefile
    also cleaned old xorg packages

--- dyneIV/packages/install-xorg.sh
+++ dyneIV/packages/install-xorg.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-DEBIAN_FRONTEND=noninteractive apt-get -q -y install \
-xserver-xorg-core xserver-xorg xinit xterm xserver-xorg-video-dummy \
-xserver-xephyr x11-utils \
-openbox python3-xdg \
-mesa-utils desktop-base devuan-baseconf xdm devuan-xdm-config-override \
-deepsea-icon-theme mate-desktop-environment mate-desktop-environment-extras \
-engrampa atril mate-applet-brisk-menu pluma eom mozo
-
 # fallback wm is openbox
 mkdir -p /etc/X11
 echo "exec openbox-session" > /etc/X11/xinitrc
