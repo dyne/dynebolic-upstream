494d305993d574def71f8d2b8b4c6534c30f33c2
Jaromil <jaromil@dyne.org>
2024-01-08 22:36:40 +0000

    fix install-locale and paths for schroot scripts

--- dyneIV/bootstrap/install-locale.sh
+++ dyneIV/bootstrap/install-locale.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 locale-gen --purge en_US.UTF-8
-echo -e \
-	 'LANG="en_US.UTF-8"\nLANGUAGE="en_US:en"\nLC_ALL="en_US.UTF-8\n"' \
+echo \
+	 'LANG="en_US.UTF-8"\nLANGUAGE="en_US:en"\nLC_ALL="en_US.UTF-8"\n' \
 	 > /etc/default/locale
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 dpkg-reconfigure --frontend=noninteractive locales
