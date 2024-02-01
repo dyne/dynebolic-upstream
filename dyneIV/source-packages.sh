d26c031fd0236ffa09a8b23c8c5a480875d4f1a6
Jaromil <jaromil@dyne.org>
2024-02-01 21:56:51 +0100

    target to download all source packages in apt

---
+++ dyneIV/source-packages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+# ${Source} doesn't always show the source package name, ${source:Package} does.
+# Multiple packages can have the same source, sort -u eliminates duplicates.
+apt-get -q -y update
+mkdir -p /usr/src/source-packages
+chown -R _apt /usr/src/source-packages
+pushd /usr/src/source-packages
+
+dpkg-query -f '${source:Package}\n' -W | sort -u | while read p; do
+	# excludes
+    [ "$p" == "freesh-archive-keyring" ] && continue
+
+    mkdir -p $p && chown -R _apt $p
+    pushd $p
+
+    # -qq very quiet, pushd provides cleaner progress.
+    # -d download compressed sources only, do not extract.
+    apt-get -qq -d source $p
+
+    popd
+done
+
+popd
