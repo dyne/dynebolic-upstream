c525b8911b53ab8f07023a55d0a9ef602dd41aab
vortex <adamb@free2air.net>
2024-02-12 09:23:52 +0100

    tidies eatmydata builder patch script

--- dyneIV/patch-eatmydata.sh
+++ dyneIV/patch-eatmydata.sh
@@ -1,2 +1,4 @@
 #/bin/bash
-patch /usr/share/mmdebstrap/hooks/eatmydata/customize.sh ./customize.sh.patch
+# execute on build system
+# adds no-pager option to allow unattended dynebolic builds
+patch /usr/share/mmdebstrap/hooks/eatmydata/customize.sh ./eatmydata-customize.sh.patch
