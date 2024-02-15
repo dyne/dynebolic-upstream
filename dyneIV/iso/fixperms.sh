cf1309121debc90ad7add105062762b12c54b1a0
Jaromil <jaromil@dyne.org>
2024-02-15 08:27:07 +0100

    fix permissions with suid bits

--- dyneIV/iso/fixperms.sh
+++ dyneIV/iso/fixperms.sh
@@ -1,27 +1,95 @@
 #!/bin/bash
-
+set -e
 # To check which files need restoring perms look into static
 # also look for unusual perms into ROOT/etc
 # sudo find ROOT/etc -printf '%m %p\0' | xargs --null -n1 | grep -v 777 | grep -v 755 | grep -v 644
 
->&2 echo "Fixing permissions in ROOT folders..."
-chown -R root:root /etc /home /lib /lib64 \
-	  /media /mnt /opt /root /run /sbin /srv /var
+#>&2 echo "Fixing permissions in ROOT folders..."
+chown -R root:root /root
+chmod 700 /root
+chown -R dyne:dyne /home/dyne
+chmod 700 /home/dyne
+# chown clears suid & guid permission bits occasionally set under /bin & /usr/bin
+find /etc /home /lib /lib64 /media /mnt /opt /sbin /srv /usr /var \
+	 -not -path /usr/share -not -path /var/cache/apt \
+	 \! -user root -o \! -group root -exec \
+	 chown root:root '{}' \;
+# find / -not -path /proc -type d -exec chmod go-w '{}' \;
+# find /bin \! -user root -o \! -group root -exec chown root:root '{}' \;
+# find /usr \! -user root -o \! -group root -exec chown root:root '{}' \;
+# chmod -R go-w      /etc /lib /lib64 /opt /bin /sbin /usr/bin /usr/sbin
+# Set particular ownerships
+chown root:messagebus \
+	/usr/lib/dbus-1.0/dbus-daemon-launch-helper
+chown root:staff \
+    /usr/local/share/fonts
+chown root:utmp \
+    /usr/lib/x86_64-linux-gnu/utempter/utempter
+chown root:tty \
+    /usr/lib/mc/cons.saver \
+    /usr/bin/write \
+    /usr/bin/wall \
+    /usr/bin/ssh-agent
+chown root:shadow \
+    /usr/bin/expiry \
+    /usr/bin/chage \
+    /sbin/unix_chkpwd \
+    /etc/shadow- \
+    /etc/shadow \
+    /etc/gshadow- \
+    /etc/gshadow
+chown root:games \
+    /usr/games/tecnoballz \
+    /usr/games/lbreakout2
+chown root:crontab \
+    /usr/bin/crontab
+chown polkitd:root \
+    /usr/share/polkit-1/rules.d \
+    /etc/polkit-1/rules.d
 
-# chown clears suid & guid permission bits occasionally set under /bin & /usr/bin, only change ownership when not root:root already
-
-find /bin \! -user root -o \! -group root -exec chown root:root '{}' \;
-find /usr \! -user root -o \! -group root -exec chown root:root '{}' \;
-
-# WARNING: SUID BIT
-chown root:messagebus /usr/lib/dbus-1.0/dbus-daemon-launch-helper
-chmod 4754 \
-	  /usr/lib/dbus-1.0/dbus-daemon-launch-helper
-# /usr/sbin/pppd
+# WARNING: granting SUID BIT
+chmod 4755 \
+    /usr/lib/polkit-1/polkit-agent-helper-1 \
+    /usr/lib/openssh/ssh-keysign \
+    /usr/bin/sudo \
+    /usr/bin/schroot \
+    /usr/bin/pkexec \
+    /usr/bin/passwd \
+    /usr/bin/newuidmap \
+    /usr/bin/newgrp \
+    /usr/bin/newgidmap \
+    /usr/bin/gpasswd \
+    /usr/bin/chsh \
+    /usr/bin/chfn \
+    /bin/umount \
+    /bin/su \
+    /bin/ntfs-3g \
+    /bin/mount \
+    /bin/fusermount3 \
+    /usr/lib/dbus-1.0/dbus-daemon-launch-helper
+chmod 2775 \
+    /usr/local/share/fonts
+chmod 2755 \
+    /usr/lib/x86_64-linux-gnu/utempter/utempter \
+    /usr/lib/mc/cons.saver \
+    /usr/bin/write \
+    /usr/bin/wall \
+    /usr/bin/ssh-agent \
+    /usr/bin/expiry \
+    /usr/bin/chage \
+    /sbin/unix_chkpwd \
+    /usr/games/tecnoballz \
+    /usr/games/lbreakout2 \
+    /usr/bin/crontab
+chmod 1777 \
+    /run/screen \
+    /run/lock
 # chmod 3775 \
 # 	  /usr/share/ppd/custom
 
-chown -R dyne:dyne /home/dyne
+
+#####
+# /etc
 chmod 755 /etc/sddm.conf.d \
 	  /etc/sysctl.d \
 	  /etc/sudoers.d \
