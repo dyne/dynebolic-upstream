74190fe149e6982f492f2b6ce2102f16f50c2b8d
Jaromil <jaromil@dyne.org>
2024-03-20 11:19:30 +0100

    update grub and try to fix iso docking

--- dyneIV/config.mk
+++ dyneIV/config.mk
@@ -1,5 +1,26 @@
+# Copyright (C) 2023-2024 Dyne.org Foundation
+#
+# Designed, written and maintained by Denis Roio <jaromil@dyne.org>
+#
+# This source code is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This source code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	Please refer
+# to the GNU Public License for more details.
+#
+# You should have received a copy of the GNU Public License along with
+# this source code; if not, , see <https://www.gnu.org/licenses/>.
+
 FILEPFX ?= dyneIV
 ARCH ?= amd64
+
+# TODO: live-boot supports only .squashfs extension
+# MODEXT ?= dyne
+
 # temporary rootfs in RAM to speed up and avoid ssd usage
 # ROOT ?= /dev/shm/dynebolic-rootfs
 ROOT ?= $(shell git rev-parse --show-toplevel)/dyneIV/ROOT
