6e6252d719117886c5c661aacc5eea2325748055
Jaromil <jaromil@dyne.org>
2024-07-10 12:23:24 +0200

    hold libpulse-mainloop-glib0 for X

--- dyneIV/modules/multimedia-script.sh
+++ dyneIV/modules/multimedia-script.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+# libpulse.mainloop-glib cannot be removed or X won't start
+# but debian says "it's unused" and auto wipes it, so we pin it
+# fruity, 10 July 2024
+apt-mark hold libpulse-mainloop-glib0
 
-[ -r "/usr/local/bin/libreoffice24.2" ] && {
-	>&2 echo "-- Libreoffice found already installed."
-	exit 0
-}
 
-wget https://files.dyne.org/dynebolic/development/LibreOffice_24.2.1_Linux_x86-64_deb.tar.gz
-tar xf LibreOffice_24.2.1_Linux_x86-64_deb.tar.gz \
-&& cd LibreOffice_24.2.1.2_Linux_x86-64_deb/DEBS \
-&& dpkg -i *.deb
 
-rm -rf LibreOffice*
+#############
+# LIBREOFFICE
+#[ -r "/usr/local/bin/libreoffice24.2" ] && {
+#	>&2 echo "-- Libreoffice found already installed."
+#	exit 0
+#}
+
+#wget https://files.dyne.org/dynebolic/development/LibreOffice_24.2.1_Linux_x86-64_deb.tar.gz
+#tar xf LibreOffice_24.2.1_Linux_x86-64_deb.tar.gz \
+#&& cd LibreOffice_24.2.1.2_Linux_x86-64_deb/DEBS \
+#&& dpkg -i *.deb
+
+#rm -rf LibreOffice*
+
+
+
