54d67294c90f73947dd81bb61f8b94a289ca12ed
Jaromil <jaromil@dyne.org>
2024-07-10 18:40:40 +0200

    fixes to module build scripts

--- dyneIV/modules/multimedia-script.sh
+++ dyneIV/modules/multimedia-script.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# libpulse.mainloop-glib cannot be removed or X won't start
-# but debian says "it's unused" and auto wipes it, so we pin it
-# fruity, 10 July 2024
-apt-mark hold libpulse-mainloop-glib0
-
 
 # missing asound conf to use pipewire
 # fruity, 10 July 2024
-cat < EOF > /etc/asoundrc
+cat << EOF > /etc/asoundrc
 pcm.!default pipewire
 ctl.!default pipewire
 EOF
