a05bfd9d90943300a321245b5f6620baf8025f38
Jaromil <jaromil@dyne.org>
2024-07-11 11:26:55 +0200

    add kde script file

---
+++ dyneIV/modules/kde-script.sh
@@ -0,0 +1,12 @@
+
+
+# libpulse.mainloop-glib cannot be removed or X won't start
+# but debian says "it's unused" and auto wipes it, so we pin it
+# fruity, 10 July 2024
+apt-mark hold libpulse-mainloop-glib0
+
+
+# flatpak is activated by dyne-install on user request (requires storage and awareness)
+# HOME=/home/dyne setuidgid dyne \
+#	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
