3ae42f507628f1625bfd6274603d3ab147190c06
fruity <root@localhost>
2024-09-30 22:43:59 +0200

    Added install audio script for pipewire 1.2.4 from debian-backports

--- dyneIV/system/install-audio.sh
+++ dyneIV/system/install-audio.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+echo "deb http://deb.debian.org/debian bookworm-backports main contrib non-free" > /etc/apt/sources.list.d/pipewire-backport.list
+
+apt-get -q update
+
+DEBIAN_FRONTEND=noninteractive apt-get -t bookworm-backports -q -y --reinstall install \
+	pipewire-audio pipewire pipewire-pulse pipewire-jack pipewire-bin pipewire-alsa \
+	wireplumber pipewire-libcamera pipewire-v4l pipewire-tests
+
 DEBIAN_FRONTEND=noninteractive apt-get -q -y install \
-pipewire-audio pavucontrol pamix \
-audacity hydrogen zynaddsubfx
+	pavucontrol pamix audacity hydrogen zynaddsubfx
+
+rm /etc/apt/sources.list.d/pipewire-backport.list
+
+apt-get -q update
 
 # https://wiki.gentoo.org/wiki/PipeWire
 # https://gitweb.gentoo.org/repo/gentoo.git/tree/media-video/pipewire/files/gentoo-pipewire-launcher.in-r3
