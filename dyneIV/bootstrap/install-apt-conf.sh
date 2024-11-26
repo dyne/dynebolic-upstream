26fd35ed1470e165dccada619f8490d5d7b45baf
Fruity <fruity@dyne.org>
2024-11-26 09:16:52 +0100

    Dangling apt configs, added proper script to install and upgrade pipewire to upstream

--- dyneIV/bootstrap/install-apt-conf.sh
+++ dyneIV/bootstrap/install-apt-conf.sh
@@ -4,9 +4,11 @@ echo "Acquire::Retries \"5\";" > /etc/apt/apt.conf.d/avoid-timeouts
 echo 'DPkg::options { "--force-confdef"; };' >  /etc/apt/apt.conf.d/force-confdef
 echo 'APT::Install-Recommends "false";' > /etc/apt/apt.conf.d/recommends
 echo 'APT::Install-Suggests "false";' > /etc/apt/apt.conf.d/suggests
-echo "deb http://deb.debian.org/debian bookworm-backports main" > ${ROOT}/etc/apt/sources.list.d/bookworm-backports.list
-cat << EOF > /etc/apt/preferences.d/99bookworm-backports
-Package: *
-Pin: release n=bookworm-backports
-Pin-Priority: 900
-EOF
+
+# commented out by fruity for a more granular choice down the system and modules
+#echo "deb http://deb.debian.org/debian bookworm-backports main" > ${ROOT}/etc/apt/sources.list.d/bookworm-backports.list
+#cat << EOF > /etc/apt/preferences.d/99bookworm-backports
+#Package: *
+#Pin: release n=bookworm-backports
+#Pin-Priority: 900
+#EOF
