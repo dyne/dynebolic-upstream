b97c0980982d7b7ce724762fa68a148ebfa23fb5
Fruity <fruity@dyne.org>
2024-12-10 20:15:53 +0100

    Wrong kernel package name-version

--- dyneIV/bootstrap/install-kernel-libre.sh
+++ dyneIV/bootstrap/install-kernel-libre.sh
@@ -8,9 +8,8 @@ dpkg -i /usr/src/${FREESH}
 rm -f /usr/src/${FREESH}
 DEBIAN_FRONTEND=noninteractive apt-get update -q -y
 DEBIAN_FRONTEND=noninteractive apt-get install -q -y \
-	linux-image-6.6.63-gnu linux-headers-6.6.63-gnu
+	linux-image-5.15.173-gnu linux-headers-5.15.173-gnu
 
-# commented by fruity, we still need kernels on the iso, do not wipe it off
-# it will wipe the kernel down the line
-#
-# rm -f /etc/apt/sources.list.d/freesh.sources
+# fruity - i added the specification of the version for the kernel
+# and I commented the removal of the sources because we'll need it
+#rm -f /etc/apt/sources.list.d/freesh.sources
