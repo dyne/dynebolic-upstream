68fd01c420c21c08e524f4b085de17753f261a7a
Fruity <fruity@dyne.org>
2024-12-10 19:22:23 +0100

    Missing apt-get update after installing the backport definition in sources.list.d

--- dyneIV/system/install-audio.sh
+++ dyneIV/system/install-audio.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
-
-# temporarily add devuan backport repo, pin only this packages
-echo "deb http://deb.devuan.org/merged  daedalus-backports main contrib" > /etc/apt/sources.list.d/backports.list
-echo "Package: $item" >> /etc/apt/preferences.d/backports
-echo "Pin: release n=daedalus-backports" >> /etc/apt/preferences.d/backports
-echo "Pin-Priority: 900" >> /etc/apt/preferences.d/backports
-printf "\n" >> /etc/apt/preferences.d/backports
-
-apt-get -q -y update
 # All the packages that compose pipewire, they don't pull systemd
-apt-get -q -y --reinstall install -t daedalus-backports \
-gstreamer1.0-pipewire \
+PIPEPACKS="gstreamer1.0-pipewire \
 pipewire \
 pipewire-alsa \
 pipewire-audio \
@@ -21,13 +11,24 @@ pipewire-media-session \
 pipewire-pulse \
 pipewire-tests \
 pipewire-v4l2 \
-qml-module-org-kde-pipewire
+qml-module-org-kde-pipewire"
+
+# temporarily add devuan backport repo, pin only this packages
+echo "deb http://deb.devuan.org/merged  daedalus-backports main contrib" > /etc/apt/sources.list.d/pipewire-backport.list
+apt-get -q update
+
+for item in ${PIPEPACKS} ; do
+	echo "Package: $item" >> /etc/apt/preferences.d/backports
+	echo "Pin: release n=daedalus-backports" >> /etc/apt/preferences.d/backports
+	echo "Pin-Priority: 900" >> /etc/apt/preferences.d/backports
+	printf "\n" >> /etc/apt/preferences.d/backports
+	apt-get -y --reinstall install -t daedalus-backports $item
+done
+
+apt-get -q -y upgrade
 
 # clear the package repository and the cache 
 # we'll leave the pinning file so that they aren't
 # overwritten during updates
-# mv /etc/apt/sources.list.d/pipewire-backport.list /root
-rm -f /etc/apt/sources.list.d/backports.list
-
-# clean the cache
-apt-get -qy clean
+mv /etc/apt/sources.list.d/pipewire-backport.list /root
+apt-get -q update
