966983112ad07e529d640a426a8c13cf94e6385e
Jaromil <jaromil@dyne.org>
2024-12-10 15:00:53 +0100

    cleanup install-audio script

--- dyneIV/system/install-audio.sh
+++ dyneIV/system/install-audio.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
+
+# temporarily add devuan backport repo, pin only this packages
+echo "deb http://deb.devuan.org/merged  daedalus-backports main contrib" > /etc/apt/sources.list.d/backports.list
+echo "Package: $item" >> /etc/apt/preferences.d/backports
+echo "Pin: release n=daedalus-backports" >> /etc/apt/preferences.d/backports
+echo "Pin-Priority: 900" >> /etc/apt/preferences.d/backports
+printf "\n" >> /etc/apt/preferences.d/backports
+
+apt-get -q -y update
 # All the packages that compose pipewire, they don't pull systemd
-PIPEPACKS="gstreamer1.0-pipewire \
+apt-get -q -y --reinstall install -t daedalus-backports \
+gstreamer1.0-pipewire \
 pipewire \
 pipewire-alsa \
 pipewire-audio \
@@ -11,26 +21,13 @@ pipewire-media-session \
 pipewire-pulse \
 pipewire-tests \
 pipewire-v4l2 \
-qml-module-org-kde-pipewire"
-
-# temporarily add devuan backport repo, pin only this packages
-echo "deb http://deb.devuan.org/merged  daedalus-backports main contrib" > /etc/apt/sources.list.d/backports.list
-
-apt-get update -q
-
-for item in ${PIPEPACKS} ; do
-	echo "Package: $item" >> /etc/apt/preferences.d/backports
-	echo "Pin: release n=daedalus-backports" >> /etc/apt/preferences.d/backports
-	echo "Pin-Priority: 900" >> /etc/apt/preferences.d/backports
-	printf "\n" >> /etc/apt/preferences.d/backports
-	apt-get -q -y --reinstall install -t daedalus-backports $item
-done
+qml-module-org-kde-pipewire
 
 # clear the package repository and the cache 
 # we'll leave the pinning file so that they aren't
 # overwritten during updates
 # mv /etc/apt/sources.list.d/pipewire-backport.list /root
-rm /etc/apt/sources.list.d/pipewire-backport.list
+rm -f /etc/apt/sources.list.d/backports.list
 
 # clean the cache
-apt-get -q update
+apt-get -qy clean
