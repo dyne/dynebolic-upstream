0e9cd4e7c80210c9111d03dc1450e4bdcd3b987b
Fruity <fruity@dyne.org>
2024-12-23 13:30:48 +0100

    Fixed kernel name issues

--- dyneIV/bootstrap/install-kernel-libre.sh
+++ dyneIV/bootstrap/install-kernel-libre.sh
@@ -5,11 +5,17 @@ echo 'LANG="C"\nLANGUAGE="en_US:en"\nLC_ALL="C"\n' \
 
 FREESH=freesh-archive-keyring_1.1_all.deb
 dpkg -i /usr/src/${FREESH}
-rm -f /usr/src/${FREESH}
+# fruity - hold on with with rm -rf
+#rm -f /usr/src/${FREESH}
+
 DEBIAN_FRONTEND=noninteractive apt-get update -q -y
 DEBIAN_FRONTEND=noninteractive apt-get install -q -y \
-	linux-image-5.15.173-gnu linux-headers-5.15.173-gnu
+	linux-image-6.6.67-gnu linux-headers-6.6.67-gnu
 
 # fruity - i added the specification of the version for the kernel
 # and I commented the removal of the sources because we'll need it
 #rm -f /etc/apt/sources.list.d/freesh.sources
+
+# test - move linux packages to root for safekeeping
+cp /var/cache/apt/archives/linux-*-gnu-1.0_amd64.deb /root/
+
