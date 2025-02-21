8b7329775314ecbc2dfecf7bdd059479fd661755
Fruity <fruity@dyne.org>
2025-02-21 15:30:09 +0100

    Initial locale support

--- dyneIV/bootstrap/install-locale.sh
+++ dyneIV/bootstrap/install-locale.sh
@@ -1,36 +1,34 @@
 #!/bin/sh
-# Script to configure system locales non-interactively.
-# Usage: ./configure-locale.sh [LOCALE]
-# Default LOCALE is "C".
+# Script to generate and install all locales and keyboard layouts non-interactively.
 
 set -e  # Exit on error
 
-# Set default locale if not provided
-LOCALE=${1:-C}
+# Generate and install all locales
+echo "Generating and installing all locales..."
+# Uncomment all locales in /etc/locale.gen
+sed -i 's/^# \(.*\)/\1/' /etc/locale.gen
 
-# Configure /etc/default/locale
-echo "Configuring /etc/default/locale with LANG=${LOCALE}..."
-echo "LANG=\"${LOCALE}\"\nLANGUAGE=\"en_US\"\nLC_ALL=\"${LOCALE}\"\n" > /etc/default/locale
-
-# Set debconf selections for locales
-echo "Configuring debconf for locale ${LOCALE}..."
-echo "locales locales/default_environment_locale select ${LOCALE}" | debconf-set-selections
-echo "locales locales/locales_to_be_generated multiselect ${LOCALE} UTF-8" | debconf-set-selections
+# Generate locales
+locale-gen
 
 # Reconfigure locales non-interactively
 echo "Reconfiguring locales..."
 dpkg-reconfigure --frontend=noninteractive locales
 
-# Update system locale
-echo "Updating system locale to ${LOCALE}..."
-update-locale LANG=${LOCALE}
+# Configure all keyboard layouts
+echo "Configuring all keyboard layouts..."
 
-# Generate and purge locales
-echo "Generating and purging locales..."
-locale-gen --purge ${LOCALE}
+# Get a list of all available keyboard layouts
+KEYBOARD_LAYOUTS=$(localectl list-x11-keymap-layouts)
 
-# Reconfigure locales again to ensure changes are applied
-echo "Final reconfiguration of locales..."
-dpkg-reconfigure --frontend=noninteractive locales
+# Set each keyboard layout
+for layout in $KEYBOARD_LAYOUTS; do
+    echo "Setting keyboard layout: $layout"
+    setxkbmap $layout
+done
 
-echo "Locale configuration complete. System is now using ${LOCALE}."
+# Reconfigure keyboard settings
+echo "Reconfiguring keyboard settings..."
+dpkg-reconfigure --frontend=noninteractive keyboard-configuration
+
+echo "Locale and keyboard configuration complete. All locales and keyboard layouts have been installed and configured."
