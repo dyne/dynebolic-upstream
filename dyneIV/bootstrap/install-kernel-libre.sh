f3fe44867c558f89bc3e1741c5ccaa97ea8c6bd8
Fruity <fruity@dyne.org>
2025-02-21 17:12:46 +0100

    Refactored kernel installation

--- dyneIV/bootstrap/install-kernel-libre.sh
+++ dyneIV/bootstrap/install-kernel-libre.sh
@@ -8,22 +8,6 @@ echo "============================================="
 # Set locale to avoid localization issues
 echo 'LANG="C"\nLANGUAGE="en_US:en"\nLC_ALL="C"\n' > /etc/default/locale
 
-# Define the URLs to download
-URLS=(
-    "https://files.dyne.org/dynebolic/development/kernel/linux-image-6.13.3-gnu_6.13.3-gnu-3_amd64.deb"
-    "https://files.dyne.org/dynebolic/development/kernel/linux-headers-6.13.3-gnu_6.13.3-gnu-3_amd64.deb"
-    "https://files.dyne.org/dynebolic/development/kernel/linux-libc-dev_6.13.3-gnu-3_amd64.deb"
-)
-
-# Download the files to /usr/src
-echo "Downloading kernel files..."
-for url in "${URLS[@]}"; do
-    if ! wget -q -P /usr/src "$url"; then
-        echo "ERROR: Failed to download $url. Aborting installation."
-        exit 1
-    fi
-done
-
 # Install the Freesh repository keyring (if needed)
 FREESH=freesh-archive-keyring_1.1_all.deb
 if [[ -f /usr/src/${FREESH} ]]; then
@@ -34,7 +18,7 @@ fi
 DEBIAN_FRONTEND=noninteractive apt-get update -q -y
 
 # Install initramfs-tools to ensure initrd generation
-DEBIAN_FRONTEND=noninteractive apt-get install -y initramfs-tools initramfs-tools-core zstd
+DEBIAN_FRONTEND=noninteractive apt-get install -q -y initramfs-tools initramfs-tools-core zstd
 
 # Install the downloaded .deb packages
 echo "Installing downloaded .deb packages..."
