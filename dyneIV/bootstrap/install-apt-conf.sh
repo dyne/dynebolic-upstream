8685249af945772f6a47eddce43fe12dde211231
Fruity <fruity@dyne.org>
2025-02-24 08:28:21 +0100

    Starting cleanup

--- dyneIV/bootstrap/install-apt-conf.sh
+++ dyneIV/bootstrap/install-apt-conf.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# Copyright (C) 2023-2024 Dyne.org Foundation
+#
+# Designed, written and maintained by Denis Roio <jaromil@dyne.org>
+#
+# This source code is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This source code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  Please refer
+# to the GNU Public License for more details.
+#
+# You should have received a copy of the GNU Public License along with
+# this source code; if not, see <https://www.gnu.org/licenses/>.
+
 echo "Acquire::Retries \"5\";" > /etc/apt/apt.conf.d/avoid-timeouts
 echo 'DPkg::options { "--force-confdef"; };' >  /etc/apt/apt.conf.d/force-confdef
 echo 'APT::Install-Recommends "false";' > /etc/apt/apt.conf.d/recommends
--- dyneIV/system/dyne-software.sh
+++ dyneIV/system/dyne-software.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# Copyright (C) 2023-2024 Dyne.org Foundation
+#
+# Designed, written and maintained by Denis Roio <jaromil@dyne.org>
+#
+# This source code is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This source code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  Please refer
+# to the GNU Public License for more details.
+#
+# You should have received a copy of the GNU Public License along with
+# this source code; if not, see <https://www.gnu.org/licenses/>.
 
 #TMP_DEPS="gcc g++ libgcrypt20-dev qt5-qmake qtbase5-dev qtdeclarative5-dev"
 TMP_DEPS=""
